a295de41d1cdf97f0c904fe67396422c1ec119f2
mark-elias <mark7elias@gmail.com>
2024-10-18 03:30:31 -0700

    Mobile Responsive NavBar, updated styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,72 @@
+"use client";
+import { IoClose } from "react-icons/io5";
+import { HiMenu } from "react-icons/hi";
+import { useState } from "react";
 import Link from "next/link";
-import React from "react";
 
-function NavBar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="flex justify-between items-center px-5 py-3 top-0 sticky shadow-xl rounded-b-xl bg-white">
-      <div>
-        <Link href="/" className="text-2xl text-customPrimary">marcos elias</Link>
-      </div>
-      <div className="flex gap-5">
-        <Link href="/">Skills</Link>
-        <Link href="/">Education</Link>
-        <Link href="/">Projects</Link>
-        <Link href="/">Certificates</Link>
+    <nav className="bg-white top-0 sticky shadow-lg rounded-b-2xl flex justify-between p-4 items-center">
+      <Link href="/" className="text-xl font-bold text-customPrimary">
+        marcos elias
+      </Link>
+
+      {/* Mobile Button */}
+      {!isOpen ? (
+        <HiMenu
+          onClick={() => toggleMenu()}
+          className="md:hidden text-2xl hover:scale-110"
+        ></HiMenu>
+      ) : (
+        <IoClose
+          onClick={() => toggleMenu()}
+          className="md:hidden text-3xl hover:scale-110"
+        ></IoClose>
+      )}
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden absolute top-[60px] w-full flex justify-end left-0">
+          <div className="bg-zinc-100 w-[150px] mr-[14px] rounded-b-2xl shadow-inner px-3 py-5">
+            <div>
+              <Link href={"/"} className="nav-link">
+                Skills
+              </Link>
+            </div>
+            <div>
+              <Link href={"/"} className="nav-link">
+                Education
+              </Link>
+            </div>
+            <div>
+              <Link href={"/"} className="nav-link">
+                Projects
+              </Link>
+            </div>
+            <div>
+              <Link href={"/"} className="nav-link">
+                Certificates
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Desktop Links */}
+      <div className="hidden md:flex gap-6">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/contact">Contact</Link>
       </div>
     </nav>
   );
-}
+};
 
-export default NavBar;
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,57 @@
-import Link from "next/link";
 import React from "react";
 
 function HomePage() {
   return (
     <div>
-      <h1>Big Header</h1>
-      <h2>Medium Header</h2>
-      <h3>Small Header</h3>
+      HomePage
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur,
-        numquam quisquam voluptate doloribus quasi quam ullam, dolore ab impedit
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum ducimus
+        dolores doloremque tempore error necessitatibus nobis. Aspernatur
+        reiciendis minus harum repudiandae quibusdam doloremque tenetur impedit
+        sit quam, quos eos non tempora exercitationem! In velit voluptatem a
+        voluptatum temporibus minima eveniet dolor rem eaque. Praesentium sed
+        labore nulla aliquam saepe? Optio facere eligendi voluptate consequuntur
+        commodi. Maxime aut, neque quod consequatur provident, omnis laboriosam
+        expedita aliquid amet assumenda numquam natus quae dolorem, ad eum unde
+        ducimus dolores. Minima quisquam enim quo numquam! Fugit quisquam
+        deleniti blanditiis quidem itaque facere optio amet sapiente, sequi sunt
+        voluptatibus voluptas dolore nam rerum natus placeat odio. Tempore
+        exercitationem ex quaerat rerum aliquid! Suscipit, earum illo. Doloribus
+        quos ipsum culpa laudantium animi tempora veritatis fuga dolore sequi
+        quaerat provident fugiat, illum ab corporis, rem optio. Quos ipsa sequi,
+        earum rem tempore commodi dicta natus impedit reiciendis magni neque
+        rerum, veniam et. Unde hic ad laboriosam reiciendis suscipit expedita
+        sequi maiores eum quisquam mollitia illo qui voluptas temporibus ut
+        minus facere itaque accusamus consectetur, adipisci tempora facilis
+        error, rerum dolor. Sint veniam tempore consectetur sequi at hic minima
+        aut dolores praesentium architecto nulla tenetur repudiandae corrupti
+        eum, quis fugiat nemo commodi nobis aliquam nesciunt, suscipit dolorem
+        debitis. Accusamus ipsam necessitatibus voluptatibus molestiae ullam
+        tempora esse fuga labore, repellendus quisquam maxime assumenda numquam
+        id, sunt aspernatur iusto laboriosam consectetur rerum dolorem tempore
+        culpa cumque reiciendis illum temporibus! Numquam repellat, tempora
+        laboriosam ad eos cumque expedita porro beatae, nemo minus eveniet.
+        Harum debitis iusto qui illum expedita esse, pariatur dicta aut libero
+        eaque hic aspernatur tempora, ex iure voluptate optio voluptatibus
+        voluptatem! Dicta accusamus sequi nam repellat nobis praesentium
+        reprehenderit dolorem est beatae? Quas laudantium explicabo expedita
+        nostrum optio laborum molestiae mollitia facilis dolores veritatis animi
+        odio rem eius nulla numquam, eum architecto harum, itaque alias
+        excepturi dicta vitae. Magni recusandae ullam atque nostrum quae placeat
+        veritatis ex molestiae? A, officia ducimus alias quas minus dolorum
+        odit. Iste vel quos dolores commodi iure culpa pariatur unde doloremque.
+        Officia ex quae eveniet. Fuga, deleniti modi cupiditate similique quos
+        eaque dicta, possimus ad amet nisi unde nostrum mollitia exercitationem
+        expedita, ipsam consequatur laudantium animi? Nihil alias eius deleniti
+        dicta quos commodi qui similique quod voluptates odio officia corporis
+        nisi laudantium mollitia aliquid ipsam quas exercitationem harum,
+        architecto amet, repellendus optio odit voluptatem tempore!
+        Reprehenderit inventore numquam unde eum, commodi perferendis. Dolores
+        officia natus veniam aut nostrum error eveniet distinctio quam est!
       </p>
-      <Link href={"/"} className="git-link">
-        GitHub Repo
-      </Link>
-      <Link href={"/"} className="site-link">
-        View Site
-      </Link>
     </div>
   );
 }
