a8e46141f1dea408be5ac27819eb168012d7637e
mark-elias <mark7elias@gmail.com>
2024-10-18 04:37:41 -0700

    setup navigation

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -40,31 +40,31 @@ const Navbar = () => {
         <div className="lg:hidden absolute top-[62px] w-full flex justify-end left-0">
           <div className="bg-zinc-100 w-[200px] mr-[14px] rounded-b-2xl shadow-inner px-3 py-5 flex flex-col gap-1">
             <div>
-              <Link href={"/"} className="nav-link">
+              <Link href={"/skills"} className="nav-link">
                 <GiGuitarBassHead />
                 Skills
               </Link>
             </div>
             <div>
-              <Link href={"/"} className="nav-link">
+              <Link href={"/projects"} className="nav-link">
                 <FaLaptopCode />
                 Projects
               </Link>
             </div>
             <div>
-              <Link href={"/"} className="nav-link">
+              <Link href={"/education"} className="nav-link">
                 <FaGraduationCap />
                 Education
               </Link>
             </div>
             <div>
-              <Link href={"/"} className="nav-link">
+              <Link href={"/experience"} className="nav-link">
                 <FaDev />
                 Experience
               </Link>
             </div>
             <div>
-              <Link href={"/"} className="nav-link">
+              <Link href={"/certificates"} className="nav-link">
                 <TbCertificate />
                 Certificates
               </Link>
@@ -75,23 +75,23 @@ const Navbar = () => {
 
       {/* Desktop Links */}
       <div className="hidden lg:flex gap-8">
-        <Link href="/" className="nav-link">
+        <Link href="/skills" className="nav-link">
           <GiGuitarBassHead />
           Skills
         </Link>
-        <Link href="/about" className="nav-link">
+        <Link href="/projects" className="nav-link">
           <FaLaptopCode />
           Projects
         </Link>
-        <Link href="/services" className="nav-link">
+        <Link href="/education" className="nav-link">
           <FaGraduationCap className="text-lg" />
           Education
         </Link>
-        <Link href="/contact" className="nav-link">
+        <Link href="/experience" className="nav-link">
           <FaDev />
           Experience
         </Link>
-        <Link href="/contact" className="nav-link">
+        <Link href="/certificates" className="nav-link">
           <TbCertificate />
           Certificates
         </Link>
