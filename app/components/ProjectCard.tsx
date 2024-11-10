795ec8f5dac6f078c0a20ce859ebc47a04d20c76
mark-elias <mark7elias@gmail.com>
2024-11-09 23:03:46 -0800

    text size update

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -5,10 +5,16 @@ function ProjectCard() {
     <div className="project-card">
       <h3>project name</h3>
       <div className="flex flex-col lg:flex-row gap-5">
-        <div className="flex gap-3 min-w-[30%] border-2 border-red-400 overflow-auto">
+        <div className="flex gap-3 lg:min-w-[50%] border-2 border-red-400 overflow-auto">
           <img src="https://placehold.co/200x100/png"></img>
-          <img src="https://placehold.co/75x100/png"></img>
-          <img src="https://placehold.co/50x75/png"></img>
+          <img
+            src="https://placehold.co/75x100/png"
+            className="max-md:hidden"
+          ></img>
+          <img
+            src="https://placehold.co/50x75/png"
+            className="max-md:hidden"
+          ></img>
         </div>
         <p className="flex flex-wrap">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad a quam
@@ -16,13 +22,12 @@ function ProjectCard() {
           dicta, neque modi temporibus quas inventore dignissimos molestiae ad?
         </p>
       </div>
-      <div className="flex gap-3">
-        <a className="cool-link">javascript</a>
-        <a className="cool-link">javascript</a>
-        <a className="cool-link">javascript</a>
-        <a className="cool-link">javascript</a>
-        <a className="cool-link">javascript</a>
-        <a className="cool-link">javascript</a>
+      <div className="flex flex-wrap gap-1 mt-5">
+        <a className="cool-span">javascript</a>
+        <a className="cool-span">javascript</a>
+        <a className="cool-span">javascript</a>
+        <a className="cool-span">javascript</a>
+        <a className="cool-span">javascript</a>
       </div>
     </div>
   );
