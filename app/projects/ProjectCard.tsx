91999075c20cbe4fa1e1f0b7af0615b0ce2b77c3
mark-elias <mark7elias@gmail.com>
2024-11-12 15:56:49 -0800

    make custom hook for Badge Maker

--- app/projects/ProjectCard.tsx
+++ app/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { useBadgeMaker } from "@/hooks/useBadgeMaker";
 import Image from "next/image";
 import React from "react";
 
@@ -11,56 +12,6 @@ interface Props {
   badges: string[];
 }
 
-// Define background colors for each technology
-const badgeMaker: {
-  [key: string]: { backgroundColor: string; textColor: string };
-} = {
-  // Front End
-  "React.js": { backgroundColor: "#61DAFB", textColor: "#000000" },
-  TypeScript: { backgroundColor: "#007ACC", textColor: "#FFFFFF" },
-  Tailwind: { backgroundColor: "#06B6D4", textColor: "#FFFFFF" },
-
-  // Full Stack & Back End
-  "Next.js": { backgroundColor: "#000000", textColor: "#FFFFFF" },
-  "Node.js": { backgroundColor: "#339933", textColor: "#FFFFFF" },
-  "Express.js": { backgroundColor: "#000000", textColor: "#FFFFFF" },
-
-  // Forms & Data Validation
-  "React-Hook-Form": { backgroundColor: "#EC5990", textColor: "#FFFFFF" },
-  Zod: { backgroundColor: "#3F3D56", textColor: "#FFFFFF" },
-  Joi: { backgroundColor: "#0A8A94", textColor: "#FFFFFF" },
-
-  // APIs & Data Fetching
-  "REST API's": { backgroundColor: "#f07f24", textColor: "#FFFFFF" },
-  "API Development": { backgroundColor: "#1e293b", textColor: "#FFFFFF" },
-  "TanStack Query": { backgroundColor: "#FF4154", textColor: "#FFFFFF" },
-  Postman: { backgroundColor: "#FF6C37", textColor: "#FFFFFF" },
-  Axios: { backgroundColor: "#5A29E4", textColor: "#FFFFFF" },
-
-  // Version Control
-  Git: { backgroundColor: "#F05032", textColor: "#FFFFFF" },
-  GitHub: { backgroundColor: "#181717", textColor: "#FFFFFF" },
-
-  // Databases
-  MongoDB: { backgroundColor: "#47A248", textColor: "#FFFFFF" },
-  mySQL: { backgroundColor: "#00758F", textColor: "#FFFFFF" },
-  PostgreSQL: { backgroundColor: "#336791", textColor: "#FFFFFF" },
-
-  // ORMs
-  Mongoose: { backgroundColor: "#880000", textColor: "#FFFFFF" },
-  Prisma: { backgroundColor: "#0C344B", textColor: "#FFFFFF" },
-
-  // Authentication
-  Clerk: { backgroundColor: "#3B82F6", textColor: "#FFFFFF" },
-  NextAuth: { backgroundColor: "#000000", textColor: "#FFFFFF" },
-
-  // Deployment & Hosting
-  Vercel: { backgroundColor: "#000000", textColor: "#FFFFFF" },
-  Render: { backgroundColor: "#46E3B7", textColor: "#000000" },
-  Hostinger: { backgroundColor: "#3E1DDB", textColor: "#FFFFFF" },
-  "GitHub Pages": { backgroundColor: "#181717", textColor: "#FFFFFF" },
-};
-
 function ProjectCard({
   image,
   alt,
@@ -70,6 +21,9 @@ function ProjectCard({
   githubLink,
   badges,
 }: Props) {
+  // Hook for making badges
+  const badgeStyles = useBadgeMaker(badges);
+
   return (
     <div className="project-card">
       <div className="w-full h-[150px] relative">
@@ -103,24 +57,18 @@ function ProjectCard({
           </a>
         </div>
         <div className="flex flex-wrap gap-1 mt-5 text-sm">
-          {badges.map((b) => {
-            const colors = badgeMaker[b] || {
-              backgroundColor: "#ccc",
-              textColor: "#000",
-            }; // Default colors if not found
-            return (
-              <span
-                key={b}
-                className="cool-badge"
-                style={{
-                  backgroundColor: colors.backgroundColor,
-                  color: colors.textColor,
-                }}
-              >
-                {b}
-              </span>
-            );
-          })}
+          {badgeStyles.map((badgeStyle) => (
+            <span
+              key={badgeStyle.name}
+              className="cool-badge"
+              style={{
+                backgroundColor: badgeStyle.styles.backgroundColor,
+                color: badgeStyle.styles.textColor,
+              }}
+            >
+              {badgeStyle.name}
+            </span>
+          ))}
         </div>
       </div>
     </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,6 +15,60 @@ function ProjectsPage() {
           githubLink="https://youtube.com"
           badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
         ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
+        <ProjectCard
+          image="/images/expenseTracker_screenshot.png"
+          alt="expense tracker screenshot"
+          title="Expense Tracker"
+          description="This project is a sample project example description for this project card example testing project card"
+          siteLink="https://google.com"
+          githubLink="https://youtube.com"
+          badges={["React.js", "Next.js", "Git", "Postman", "MongoDB", "mySQL", "PostgreSQL"]}
+        ></ProjectCard>
       </div>
     </section>
   );
