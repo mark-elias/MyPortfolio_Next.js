e6aae627303ad9573663cc5c3d74431f4485a960
mark-elias <mark7elias@gmail.com>
2024-11-12 17:26:31 -0800

    update Skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,68 +1,45 @@
 import React from "react";
-
+import BadgeList from "./BadgeList";
 function SkillsPage() {
   return (
-    <div>
-      SkillsPage
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
-        doloremque enim soluta fugit praesentium, reiciendis voluptates quas
-        quidem dolorum recusandae amet, animi doloribus suscipit tenetur
-        pariatur eos beatae temporibus ducimus aperiam. Dolorum velit doloremque
-        sed, recusandae dolor, ipsam officia consectetur eaque dolorem
-        provident, aliquid nam? Iste reiciendis veniam accusamus expedita alias
-        aliquam laborum maxime culpa. Placeat vitae fuga culpa voluptas
-        blanditiis minus facilis iste impedit quia, modi minima cumque aut
-        fugiat voluptatibus labore, optio molestiae? Dolores amet error ex
-        repellendus nihil, qui ut cupiditate veritatis enim non, corporis
-        accusantium. Voluptatibus deserunt qui, quo eveniet doloribus fugit
-        aperiam, ipsa perferendis dicta obcaecati consectetur. Amet voluptas,
-        numquam enim quas aperiam magni? Dolorum deserunt id est vitae similique
-        odio magnam iste repellendus, consequuntur molestiae nam inventore
-        cupiditate mollitia ipsam quae iure, dolores voluptas blanditiis? Saepe,
-        magnam laborum sint voluptatibus inventore ipsum incidunt! Vero eius
-        tempora exercitationem? Expedita, quos architecto, explicabo porro totam
-        sed vel mollitia officia facere tenetur, qui minima in rem odit ea
-        consequuntur quam pariatur laborum praesentium fuga similique! Iste esse
-        magnam tempore excepturi! Eaque nobis officia culpa cupiditate voluptas
-        ab velit ipsum mollitia doloribus! Magni a labore nobis neque quibusdam
-        maxime, dolore beatae error voluptas deserunt? Minus nisi laborum
-        praesentium perspiciatis qui a modi optio deserunt consequuntur
-        repellendus sit eum mollitia velit, veniam illum quidem et debitis at
-        harum earum inventore nostrum illo. Iure quod consectetur rerum? Eveniet
-        magnam impedit, quo sit quae assumenda voluptate soluta ea in enim
-        dolorum harum, ducimus commodi aliquid quibusdam voluptatum laboriosam
-        esse! Blanditiis quaerat, dolorem quisquam eveniet beatae, similique
-        tempora molestias placeat dicta sapiente praesentium sunt iusto est ab!
-        Hic fugiat cumque quibusdam quisquam excepturi, molestiae, minus
-        perferendis quod nulla dicta blanditiis voluptas, eum dolorum. Ad ea
-        suscipit accusantium quisquam neque nam reiciendis rerum repudiandae
-        aspernatur, temporibus nesciunt odit nostrum explicabo debitis similique
-        doloribus itaque, enim aliquam. Quaerat deleniti nostrum nisi fuga
-        explicabo laborum consectetur quo eos rem excepturi vero tempore alias
-        rerum provident dolorem, animi repellendus reprehenderit officia porro.
-        Ea totam placeat eum commodi alias quod laudantium iusto minus incidunt,
-        nostrum officia eveniet molestias nesciunt minima magnam iste nisi
-        voluptatem optio dolores. Dolore ipsa reprehenderit ex recusandae
-        laborum laudantium excepturi, architecto quo odio doloremque tempore
-        nisi vitae temporibus quam, repudiandae atque ipsum maxime. Corrupti,
-        repellat laborum autem ad quibusdam esse numquam iste, in quam suscipit
-        ipsa labore consequuntur doloremque, cupiditate consectetur nihil
-        recusandae. Necessitatibus sequi at officia modi nisi corrupti dolor
-        dolorem eos placeat, cupiditate dolorum minima aliquam doloribus dolores
-        molestias! Voluptate consectetur natus, obcaecati optio ipsam ipsum
-        sapiente. Deleniti dolores, odio magni aperiam molestias eligendi
-        officia asperiores adipisci recusandae pariatur fuga laborum inventore
-        sunt explicabo praesentium, cumque voluptate distinctio repudiandae
-        nesciunt. Iusto nemo voluptas unde et ratione? Explicabo, sed suscipit
-        debitis aut mollitia voluptatem. Sapiente, consequuntur omnis eius
-        similique alias, natus praesentium corrupti quo eaque, necessitatibus
-        animi. Facere dicta sapiente impedit, accusamus vel, quod, nostrum nulla
-        totam beatae eos quis adipisci! Nisi natus ratione ab illo, quibusdam
-        molestias optio harum necessitatibus dignissimos et. Aperiam quas alias
-        sunt ullam delectus animi quibusdam atque.
-      </p>
-    </div>
+    <section>
+      <h1>Skills</h1>
+
+      <div className="p-5">
+        <h2>Front End</h2>
+        <BadgeList badgeList={["React", "TypeScript", "Tailwind"]} />
+        <h2>Full Stack and Back End</h2>
+        <BadgeList badgeList={["Next.js", "Node.js", "Express.js"]} />
+        <h2>Forms & Data Validation</h2>
+        <BadgeList badgeList={["React-Hook-Form", "Zod", "Joi"]} />
+        <h2>API's & Data Fetching</h2>
+        <BadgeList
+          badgeList={[
+            "REST API's",
+            "API Development",
+            "TanStack Query",
+            "Postman",
+            "Axios",
+          ]}
+        />
+        <h2>Version Control</h2>
+        <BadgeList badgeList={["Git", "GitHub"]} />
+        <h2>Databases</h2>
+        <BadgeList badgeList={["MongoDB", "mySQL", "PostgreSQL"]} />
+        <h2>ORM's</h2>
+        <BadgeList badgeList={["Mongoose", "Prisma"]} />
+        <h2>Authentication</h2>
+        <BadgeList badgeList={["Clerk", "NextAuth"]} />
+        <h2>Deployment & Hosting</h2>
+        <BadgeList
+          badgeList={["Vercel", "Render", "Hostinger", "GitHub Pages"]}
+        />
+        <p className="mt-8">
+          👍 Still want to learn these: Stripe payments, Supabase, Shadcn UI,
+          React Native, Deno, Bun
+        </p>
+      </div>
+    </section>
   );
 }
 
