a6f29eb929475a63cd0f5bcd289ecc5a71ad7843
mark-elias <melias23@proton.me>
2025-08-03 15:59:14 -0700

    create Summary constants file

--- src/components/organisms/Summary.tsx
+++ src/components/organisms/Summary.tsx
@@ -1,29 +1,13 @@
-import React from "react";
+// constants
+import { summaryText } from "@/src/constants/summary.constants";
 
 function Summary() {
   return (
     <section className="rounded-lg custom-shadow md:min-w-[500px]">
-      <p>
-        Full Stack Developer with expertise in building responsive,
-        user-friendly web applications using Next.js, TypeScript, and Node.js.
-      </p>
-      <p>
-        Experienced in frontend development using Next.js, React, Tailwind, and
-        Atomic Design principles to create reusable components and seamless user
-        interfaces. Skilled in backend development with Node.js, Nest.js, and
-        MongoDB, building RESTful APIs, handling business logic, and managing
-        databases.
-      </p>
-      <p>
-        Proficient in form validation with React Hook Form and Zod, and API
-        development & data fetching using Axios, TanStack Query, and Postman.
-        Knowledgable in Git for version control and experienced in remote
-        collaboration using Figma, Slack, and Jira.
-      </p>
-      <p className="mt-10">
-        🤠 Hobbies: Watching sports 🏈, going to concerts 🎶, and playing the
-        guitar 🎸
-      </p>
+      <p>{summaryText.summary1}</p>
+      <p>{summaryText.summary2}</p>
+      <p>{summaryText.summary3}</p>
+      <p className="mt-10">{summaryText.summary4}</p>
     </section>
   );
 }
---
+++ src/constants/summary.constants.ts
@@ -0,0 +1,10 @@
+export const summaryText = {
+  summary1:
+    "Full Stack Developer with expertise in building responsive, user-friendly web applications using Next.js, TypeScript, and Node.js.",
+  summary2:
+    "Experienced in frontend development using Next.js, React, Tailwind, and Atomic Design principles to create reusable components and seamless user interfaces. Skilled in backend development with Node.js, Nest.js, and MongoDB, building RESTful APIs, handling business logic, and managing databases.",
+  summary3:
+    "Proficient in form validation with React Hook Form and Zod, and API development & data fetching using Axios, TanStack Query, and Postman. Knowledgable in Git for version control and experienced in remote collaboration using Figma, Slack, and Jira.",
+  summary4:
+    "🤠 Hobbies: Watching sports 🏈, going to concerts 🎶, and playing the guitar 🎸",
+};
