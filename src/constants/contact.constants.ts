7b23bc3f8c158a1c219b37ef545cd11f3eff64d4
mark-elias <melias23@proton.me>
2025-08-03 16:57:15 -0700

    update contact constants

--- src/constants/contact.constants.ts
+++ src/constants/contact.constants.ts
@@ -2,5 +2,7 @@ export const contactInfo = {
   location: "San Diego",
   email: "melias44@proton.me",
   linkedin: "https://www.linkedin.com/in/marcos-elias-382484226/",
+  linkedinText: "LinkedIn",
   github: "https://github.com/mark4elias",
+  githubText: "GitHub",
 };
