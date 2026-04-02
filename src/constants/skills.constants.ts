ac85985b5130842bc438828b04368c96c96a7797
mark-elias <melias23@proton.me>
2026-04-02 05:16:55 -0700

    update Skills badge groups

--- src/constants/skills.constants.ts
+++ src/constants/skills.constants.ts
@@ -5,29 +5,22 @@ export const skillCategories = [
     name: "Languages",
     skills: ["TypeScript", "JavaScript"],
   },
+  // // runtimes
+  // {
+  //   id: "runtimes",
+  //   name: "Runtimes",
+  //   skills: ["Node.js", "Bun.js"],
+  // },
   {
     id: "frontend-frameworks",
     name: "Frontend",
-    skills: ["Next.js", "React"],
+    skills: ["React", "Next.js"],
   },
   {
     id: "backend-frameworks",
     name: "Backend",
     skills: ["Nest.js", "Express.js", "Node.js", "Bun.js"],
   },
-  // {
-  //   id: "backend-runtimes",
-  //   name: "Backend Runtimes",
-  //   skills: ["Node.js", "Bun.js"],
-  // },
-
-  // ui styling
-  {
-    id: "css-frameworks",
-    name: "CSS & UI",
-    skills: ["Tailwind", "Material UI", "shadcn/ui", "Chakra UI"],
-  },
-
   // databases
   {
     id: "databases",
@@ -35,19 +28,7 @@ export const skillCategories = [
     skills: ["MongoDB", "MySQL"],
   },
 
-  // global state management
-  {
-    id: "state-management",
-    name: "State Management",
-    skills: ["Zustand", "Redux Toolkit"],
-  },
-  // {
-  //   id: "orms-odms",
-  //   name: "ODMs & ORMs",
-  //   skills: ["Mongoose", "Prisma"],
-  // },
-
-  // apis & forms
+  // apis
   {
     id: "apis-data-fetching",
     name: "APIs & Data Fetching",
@@ -59,18 +40,58 @@ export const skillCategories = [
       "Postman",
     ],
   },
+  // auth
+  {
+    id: "authentication",
+    name: "Authentication",
+    skills: ["JWT", "Passport.js", "Cookies"],
+  },
+  // cloud and devops
+  {
+    id: "cloud-services",
+    name: "Cloud Services",
+    skills: ["AWS EC2", "AWS S3"],
+  },
+
+  // devops
+  {
+    id: "devops-infrastructure",
+    name: "DevOps & Infrastructure",
+    // skills: ["Docker", "GitHub Actions", "Nginx", "Caddy"],
+    skills: ["Docker", "GitHub Actions", "SSH", "NVM"],
+  },
+  {
+    id: "version-control",
+    name: "Version Control",
+    skills: ["Git", "GitHub"],
+  },
+  // global state management
+  {
+    id: "state-management",
+    name: "State Management",
+    skills: ["Zustand", "Redux Toolkit"],
+  },
+  // forms
   {
     id: "forms-validation",
     name: "Forms & Validation",
     skills: ["React-Hook-Form", "Zod"],
   },
 
-  // auth & payments
+  // ui styling
   {
-    id: "authentication",
-    name: "Authentication",
-    skills: ["JWT", "Passport.js", "Cookies", "bcrypt"],
+    id: "css-frameworks",
+    name: "CSS & UI",
+    skills: ["Tailwind", "Material UI", "shadcn/ui", "Chakra UI"],
   },
+
+  // {
+  //   id: "orms-odms",
+  //   name: "ODMs & ORMs",
+  //   skills: ["Mongoose", "Prisma"],
+  // },
+
+  // payments
   // {
   //   id: "payments",
   //   name: "Payments",
@@ -82,35 +103,17 @@ export const skillCategories = [
     skills: ["WebSockets"],
   },
 
-  // cloud and devops
-  {
-    id: "cloud-services",
-    name: "Cloud Services",
-    skills: ["AWS S3", "AWS EC2"],
-  },
-  {
-    id: "devops-infrastructure",
-    name: "DevOps & Infrastructure",
-    // skills: ["Docker", "GitHub Actions", "Nginx", "Caddy"],
-    skills: ["Docker", "GitHub Actions"],
-  },
-
   // dev tools
-  {
-    id: "version-control",
-    name: "Version Control",
-    skills: ["Git", "GitHub"],
-  },
   {
     id: "package-managers",
     name: "Package Managers",
-    skills: ["Yarn", "NPM", "PNPM"],
+    skills: ["Yarn", "NPM", "PNPM", "Bun"],
   },
-  {
-    id: "development-environment",
-    name: "Development Environment",
-    skills: ["Unix/Linux", "Terminal/CLI", "SSH", "Vim", "NVM"],
-  },
+  // {
+  //   id: "linux-and-environment",
+  //   name: "Linux & Server Environment",
+  //   skills: ["Unix/Linux", "Terminal/CLI", "SSH", "NVM"],
+  // },
 
   // deployment
   {
